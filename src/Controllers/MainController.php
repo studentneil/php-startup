d02e12bcae05682966267fa964c9253f566dd207
studentneil <neilo2000@gmail.com>
2016-12-07 00:18:44 +0000

    frontend work

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -8,14 +8,24 @@ class MainController
 {
     public function indexAction(Application $app)
     {
-        $collection = $app['vinyl.repository']->findAll();
         $templateName = 'home';
+        $args_array = array();
+
+        return $app['twig']->render($templateName.'.html.twig', $args_array);
+    }
+
+
+    public function getCollectionAction(Application $app)
+    {
+        $collection = $app['vinyl.repository']->findAll();
+        $templateName = 'collection';
         $args_array = array(
             'collection' => $collection,
         );
 
         return $app['twig']->render($templateName.'.html.twig', $args_array);
     }
+
 
     public function getReleaseByIdAction(Application $app, $id)
     {
