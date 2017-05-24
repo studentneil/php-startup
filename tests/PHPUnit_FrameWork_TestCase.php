60371b8469dcd82dcfb1cfbde9a4fa75f1243207
studentneil <neilo2000@gmail.com>
2017-05-24 23:19:08 +0100

    attempted polyfill for compatability issues phpunit6 and webtestcase

---
+++ tests/PHPUnit_FrameWork_TestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: neil
+ * Date: 24/05/2017
+ * Time: 23:16
+ */
+
+namespace VinylStoreTests;
+
+use PHPUnit\Framework\TestCase
+
+
+class PHPUnit_FrameWork_TestCase extends TestCase
+{
+
+}
