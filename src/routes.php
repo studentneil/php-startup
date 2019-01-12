bf2b7186ba9b04172d5a1803237e8e0cded1bbda
neil-mc-m <neilo2000@gmail.com>
2019-01-12 23:49:35 +0000

    add the trustbox

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: neil
- * Date: 26/11/2016
- * Time: 00:14.
- */
 $app->get('/', 'VinylStore\\Controllers\\MainController::indexAction');
 $app->get('/home', 'VinylStore\\Controllers\\MainController::indexAction');
 $app->get('/login', 'VinylStore\\Controllers\\LoginController::loginAction');
